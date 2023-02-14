1e308b66843e21d0b0ea8d3685a6fa61265878ae
hikaruti <nguyenphubinh2207@gmail.com>
2023-02-14 12:50:35 +0700

    add file vercel

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 // create the connection to database
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
+  host: "localhost",
   user: "root",
   password: "",
   database: "database-restaurant",
