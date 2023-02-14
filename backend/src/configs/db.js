05f63b131956379b7e0a1b6a3f814361e4b7770d
hikaruti <nguyenphubinh2207@gmail.com>
2023-02-14 11:38:45 +0700

    updata db

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -1,17 +1,31 @@
-import mysql from "mysql2";
+// import mysql from "mysql2";
+
+// // create the connection to database
+
+// const db = mysql.createConnection({
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "",
+//   database: "database-restaurant",
+// });
+
+// db.connect((error) => {
+//   if (error) throw error;
+//   console.log("Successfully connected to the database.");
+// });
+
+// export default db;
+// get the client
+import mysql from "mysql2/promise";
 
 // create the connection to database
-
-const db = mysql.createConnection({
-  host: "127.0.0.1",
+const db = mysql.createPool({
+  host: "localhost",
   user: "root",
-  password: "",
   database: "database-restaurant",
 });
 
-db.connect((error) => {
-  if (error) throw error;
-  console.log("Successfully connected to the database.");
-});
+// simple query
+// const connection = await mysql.createConnection({host:'localhost', user: 'root', database: 'test'});
 
 export default db;
