5ab87e8d51ffebd77de4a86858b5d32f5008c3fc
hikaruti <nguyenphubinh2207@gmail.com>
2023-02-15 13:39:32 +0700

    add timeout

--- backend/src/configs/db.js
+++ backend/src/configs/db.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE_NAME,
-  // connectTimeout: 30000,
+  connectTimeout: 30000,
 });
 
 db.connect((error) => {
